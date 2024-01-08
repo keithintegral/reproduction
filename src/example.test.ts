6baa7281a602b17dd99245f28cc966a86c8ec8a7
keithintegral <keith@integral.xyz>
2024-01-08 13:16:11 -0500

    Reproduce issue

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,5 +1,6 @@
 import { MikroORM } from '@mikro-orm/sqlite';
-import { User } from './user.entity';
+import { EntityA, EntityB, FieldB } from './user.entity';
+// import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 
 let orm: MikroORM;
 
@@ -15,20 +16,138 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
+  delete process.env.MIKRO_ORM_DBEUG;
   await orm.close(true);
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
+  const entityA = orm.em.create(EntityA, { organization: 'orgId' });
+  await orm.em.flush();
+
+  orm.em.create(EntityB, new EntityB('orgId', entityA, '100', 1, 1, new FieldB('anything')));
   await orm.em.flush();
   orm.em.clear();
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const filter ={
+    "organization":"orgId",
+    "entities_b":{
+       "organization":"orgId",
+       "$and":[
+          {
+             "$or":[
+                {
+                   "amount::numeric":{
+                      "$ne":0
+                   }
+                },
+                {
+                   "coalesce(fee_amount::numeric, 0)":{
+                      "$ne":0
+                   }
+                }
+             ]
+          },
+          {
+             "fieldF":false,
+             "fieldE":false
+          },
+          {
+             "$and":[
+                {
+                   "$or":[
+                      {
+                         "fieldD":{
+                            "$nin":[
+                               2,
+                               3
+                            ]
+                         }
+                      },
+                      {
+                         "fieldD":null
+                      }
+                   ]
+                },
+                {
+                   "$or":[
+                      {
+                         "fieldC":{
+                            "$nin":[
+                               2,
+                               3
+                            ]
+                         }
+                      },
+                      {
+                         "fieldC":null
+                      }
+                   ]
+                }
+             ]
+          },
+          {
+             "$or":[
+                {
+                   "fieldB":{
+                      "id":{
+                         "$nin":[
+                            "randomId1"
+                         ]
+                      }
+                   }
+                },
+                {
+                   "fieldB":null
+                }
+             ]
+          },
+          {
+             "$or":[
+                {
+                   "fieldA":false
+                }
+             ]
+          }
+       ]
+    }
+  }
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  process.env.MIKRO_ORM_DBEUG = 'true'
+  
+  const results = orm.em.getRepository(EntityA).qb()
+    .select('*')
+    .where(filter)
+    .getResult();
+  console.log('results', JSON.stringify(results))
+
+  /*
+  select 
+    `e0`.* 
+  from 
+    `entities_a` as `e0` 
+    left join `entities_b` as `e1` on `e0`.`id` = `e1`.`entity_a_id` 
+  where 
+    `e0`.`organization` = 'orgId' 
+    and `e1`.`organization` = 'orgId' 
+    and (
+      `e1`.`amount::numeric` != 0 
+      or `e1`.`coalesce(fee_amount::numeric, 0)` != 0
+    ) 
+    and `e1`.`field_f` = false 
+    and `e1`.`field_e` = false 
+    and (
+      `e1`.`field_d` not in (2, 3) 
+      or `e1`.`field_d` is null
+    ) 
+    and (
+      `e1`.`field_c` not in (2, 3) 
+      or `e1`.`field_c` is null
+    ) 
+    and (
+      `e0`.`field_b_id` not in ('randomId1') 
+      or `e1`.`field_b_id` is null
+    ) 
+    and `e1`.`field_a` = false
+  */
+      // Similar question why is `e0`.`field_b_id` not in ('randomId1') 
 });
--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,20 +1,82 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Entity, PrimaryKey, Property, ManyToOne, Index, OneToMany, Collection } from '@mikro-orm/core';
+
+@Entity({ tableName: "entities_a" })
+export class EntityA {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  organization: string;
+
+  @OneToMany({ entity: () => EntityB, mappedBy: 'entityA' })
+  entities_b = new Collection<EntityB>(this);
+
+
+  constructor(organization: string) {
+    this.organization = organization;
+  }
+
+}
+
 
 @Entity()
-export class User {
-
+export class FieldB {
   @PrimaryKey()
   id!: number;
 
   @Property()
   name: string;
 
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
+  constructor(name: string) {
     this.name = name;
-    this.email = email;
   }
+}
 
+@Entity({ tableName: "entities_b" })
+export class EntityB {
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  organization: string;
+
+  @Property()
+  amount: string;
+
+  @Property()
+  fieldE: boolean;
+
+  @Property()
+  fieldF: boolean;
+  
+  @Property({ nullable: true })
+  fieldD: number;
+
+  @Property({ nullable: true })
+  fieldC: number;
+
+  @ManyToOne({ entity: () => FieldB, nullable: true})
+  fieldB: FieldB;
+
+  @Property()
+  fieldA: boolean;
+
+  @ManyToOne({ entity: () => EntityA, nullable: true })
+  // @Index()
+  entityA?: EntityA;
+  
+
+  constructor(organization: string, entityA: EntityA, amount: string, fieldD: number, fieldC: number, fieldB: FieldB) {
+    this.organization = organization;
+    this.amount = amount;
+    this.fieldE = false;
+    this.fieldF = false;
+    this.fieldD = fieldD;
+    this.fieldC = fieldC;
+    this.fieldB = fieldB;
+    this.fieldA = false;
+    this.entityA = entityA;
+  }
 }
